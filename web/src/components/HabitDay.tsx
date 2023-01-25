8c4c809543f8a180ecdafdc68fde58a0c34b0bb4
Luiz Siqueira <36939088+Luiz-Siqueira@users.noreply.github.com>
2023-01-25 10:10:05 -0300

    Aula 2

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -3,6 +3,6 @@ interface HabitDayProps {
 }
 export function HabitDay(props: HabitDayProps) {
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-600 rounded-lg"></div>
   );
 }
--- web/src/components/SumaryTable.tsx
+++ web/src/components/SumaryTable.tsx
@@ -34,7 +34,7 @@ export function SumaryTable() {
             return (
               <div
                 key={i}
-                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-500 rounded-lg opacity-40 cursor-not-allowed"
               />
             );
           })}
