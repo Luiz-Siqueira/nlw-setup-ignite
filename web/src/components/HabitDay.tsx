41814e48a7e609a730b798c23893900acba1d649
Luiz Siqueira <36939088+Luiz-Siqueira@users.noreply.github.com>
2023-01-30 22:48:55 -0300

    Aula 3

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -15,10 +15,10 @@ export function HabitDay({ completed, amount }: HabitDayProps) {
           "w-10 h-10 bg-zinc-900 border-2 border-zinc-600 rounded-lg",
           {
             "bg-zinc-900 border-2 border-zinc-600": completedPercentage === 0 ,
-            "bg-violet-900 border-violet-500": completedPercentage > 0 && completedPercentage < 20,
-            "bg-violet-800 border-violet-500": completedPercentage >= 20 && completedPercentage < 40,
-            "bg-violet-700 border-violet-500": completedPercentage >= 40 && completedPercentage < 60,
-            "bg-violet-600 border-violet-500": completedPercentage >= 60 && completedPercentage < 80,
+            "bg-violet-900 border-violet-900": completedPercentage > 0 && completedPercentage < 20,
+            "bg-violet-800 border-violet-800": completedPercentage >= 20 && completedPercentage < 40,
+            "bg-violet-700 border-violet-700": completedPercentage >= 40 && completedPercentage < 60,
+            "bg-violet-600 border-violet-600": completedPercentage >= 60 && completedPercentage < 80,
             "bg-violet-500 border-violet-500": completedPercentage >= 80,
           }
         )}
